bdde2621a97de6bcfa6a954bbab4cce95962d51c
Kyraleese <crystalruby@gmail.com>
2012-10-27 21:36:41 -0400

    Fixed the stairs.  They'll now stay the way you laid them down after chunks get reloaded.  I only ever saw this with logging in and out, but I'd wager anything that causes chunks to unload would do it as well.

--- ModToDyeFor/TileEntityBlockDyeableStairs.java
+++ ModToDyeFor/TileEntityBlockDyeableStairs.java
@@ -1,11 +1,16 @@
 package ModToDyeFor;
 
 import net.minecraft.src.NBTTagCompound;
+import net.minecraft.src.NetworkManager;
+import net.minecraft.src.Packet;
+import net.minecraft.src.Packet130UpdateSign;
+import net.minecraft.src.Packet132TileEntityData;
 import net.minecraft.src.TileEntity;
+import net.minecraft.src.TileEntityMobSpawner;
 
 public class TileEntityBlockDyeableStairs extends TileEntity {
 
-	int stairOrientation = 2;
+	int stairOrientation = 0;
 	
     /**
      * Writes a tile entity to NBT.
@@ -21,5 +26,15 @@ public class TileEntityBlockDyeableStairs extends TileEntity {
     public void readFromNBT(NBTTagCompound par1NBTTagCompound) {
         super.readFromNBT(par1NBTTagCompound);
         this.stairOrientation = par1NBTTagCompound.getInteger("Orientation");
+    }
+    
+    public Packet getDescriptionPacket() {
+        NBTTagCompound var1 = new NBTTagCompound();
+        this.writeToNBT(var1);
+        return new Packet132TileEntityData(this.xCoord, this.yCoord, this.zCoord, 1, var1);
+    }
+    
+    public void onDataPacket(NetworkManager net, Packet132TileEntityData pkt) {
+    	this.readFromNBT(pkt.customParam1);
     }
 }
